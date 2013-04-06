1960c2f1285ff2bf350b60f40013979ea86d46a2
epic_jdog <jmward@dccnet.com>
2013-04-06 15:34:06 -0700

    Initial Commit 0.0.1

---
+++ invisimod_mainstuffs/epic_jdog/invisimod/CommonProxy.java
@@ -0,0 +1,37 @@
+package epic_jdog.invisimod;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class CommonProxy implements IGuiHandler {
+    public void registerRenderInformation() {
+    }
+    
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+    
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+    
+    public void registerTiles() {
+        
+    }
+    
+    public void registerBlocks() {
+        
+    }
+    
+    public void addNames() {
+        
+    }
+    
+    public void addRecipes() {
+        
+    }
+}
+

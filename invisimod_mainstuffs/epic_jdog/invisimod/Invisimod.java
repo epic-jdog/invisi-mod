df8eb4cb357b5fe684250fd71e42c082b2721fc8
epic_jdog <jmward@dccnet.com>
2013-04-06 18:19:10 -0700

    Added rece-pie!

--- invisimod_mainstuffs/epic_jdog/invisimod/Invisimod.java
+++ invisimod_mainstuffs/epic_jdog/invisimod/Invisimod.java
@@ -1,6 +1,7 @@
 package epic_jdog.invisimod;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -11,8 +12,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 @Mod(modid=Invisimod.modID, name="Invisimod", version="0.0.1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
@@ -38,6 +41,14 @@ public class Invisimod {
                 proxy.registerRenderInformation();
         
                 LanguageRegistry.addName(invisidustItem,"Invisi-Dust");
+        
+                ItemStack glowstoneStack = new ItemStack(Item.lightStoneDust);
+                ItemStack gunpowdahStack = new ItemStack(Item.gunpowder);
+                ItemStack goldenCarrotStack = new ItemStack(Item.goldenCarrot);
+
+                GameRegistry.addRecipe(new ItemStack(Invisimod.invisidustItem), "x  ", "  y", " z ",
+                        'x', glowstoneStack, 'y', gunpowdahStack, 'z', goldenCarrotStack);
+        
         }
         
         @PostInit

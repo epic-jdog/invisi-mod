1f1e3e561d59cbf77a4eb7b3f7194bfd8a8b709f
Rekkyn <reedesler@gmail.com>
2013-04-06 21:03:24 -0700

    Fixed armour renering and clean up code

--- invisimod_mainstuffs/epic_jdog/invisimod/InvisiArmorItem.java
+++ invisimod_mainstuffs/epic_jdog/invisimod/InvisiArmorItem.java
@@ -1,23 +1,24 @@
 package epic_jdog.invisimod;
 
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.common.IArmorTextureProvider;
+import net.minecraftforge.common.ISpecialArmor;
 
-public class InvisiArmorItem extends ItemArmor{
-
-    public InvisiArmorItem(int id, EnumArmorMaterial material,
-            int vlee, int armorslot) {
+public class InvisiArmorItem extends ItemArmor implements IArmorTextureProvider {
+    
+    public InvisiArmorItem(int id, EnumArmorMaterial material, int vlee, int armorslot) {
         super(id, material, armorslot, armorslot);
-        // TODO Auto-generated constructor stub
     }
-public String getArmorTextureFile (ItemStack itemstack) {
-    return "/mods/Invisimod/armor/invisiarmor_1.png";
-}
-
-
-
-
-
-
+    
+    @Override
+    public String getArmorTextureFile(ItemStack itemstack) {
+        return "/mods/Invisimod/armor/Invisiarmor.png";
+    }
+    
+    
 }
